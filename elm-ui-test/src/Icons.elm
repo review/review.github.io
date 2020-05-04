a928d28cdc6541dde6915f81cc48dfaf05233003
Anthony J. Clark <ajc@>
2020-05-04 02:31:14 -0500

    Added vehicle example.

---
+++ elm-ui-test/src/Icons.elm
@@ -0,0 +1,148 @@
+-- https://1602.github.io/elm-feather-icons/
+-- play (playing)
+-- pause (paused)
+-- repeat (loop playback)
+-- filePlus (link, open file, drop zone)
+-- trash2 (delete current)
+-- loader (while loading)
+-- crosshair (follow)
+-- video (reset camera)
+-- eye (toggle colors)
+-- download (download gltf)
+-- github (to github)
+
+
+module Icons exposing
+    ( crosshair
+    , download
+    , eye
+    , filePlus
+    , github
+    , loader
+    , pause
+    , play
+    , repeat
+    , trash2
+    , video
+    )
+
+import Html exposing (Html)
+import Svg exposing (Svg, svg)
+import Svg.Attributes exposing (..)
+
+
+svgFeatherIcon : String -> List (Svg msg) -> Html msg
+svgFeatherIcon className =
+    svg
+        [ class <| "feather feather-" ++ className
+        , fill "none"
+        , height "24"
+        , stroke "currentColor"
+        , strokeLinecap "round"
+        , strokeLinejoin "round"
+        , strokeWidth "2"
+        , viewBox "0 0 24 24"
+        , width "24"
+        ]
+
+
+crosshair : Html msg
+crosshair =
+    svgFeatherIcon "crosshair"
+        [ Svg.circle [ cx "12", cy "12", r "10" ] []
+        , Svg.line [ x1 "22", y1 "12", x2 "18", y2 "12" ] []
+        , Svg.line [ x1 "6", y1 "12", x2 "2", y2 "12" ] []
+        , Svg.line [ x1 "12", y1 "6", x2 "12", y2 "2" ] []
+        , Svg.line [ x1 "12", y1 "22", x2 "12", y2 "18" ] []
+        ]
+
+
+download : Html msg
+download =
+    svgFeatherIcon "download"
+        [ Svg.path [ d "M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" ] []
+        , Svg.polyline [ points "7 10 12 15 17 10" ] []
+        , Svg.line [ x1 "12", y1 "15", x2 "12", y2 "3" ] []
+        ]
+
+
+eye : Html msg
+eye =
+    svgFeatherIcon "eye"
+        [ Svg.path [ d "M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" ] []
+        , Svg.circle [ cx "12", cy "12", r "3" ] []
+        ]
+
+
+filePlus : Html msg
+filePlus =
+    svgFeatherIcon "file-plus"
+        [ Svg.path [ d "M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" ] []
+        , Svg.polyline [ points "14 2 14 8 20 8" ] []
+        , Svg.line [ x1 "12", y1 "18", x2 "12", y2 "12" ] []
+        , Svg.line [ x1 "9", y1 "15", x2 "15", y2 "15" ] []
+        ]
+
+
+github : Html msg
+github =
+    svgFeatherIcon "github"
+        [ Svg.path [ d "M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" ] []
+        ]
+
+
+loader : Html msg
+loader =
+    svgFeatherIcon "loader"
+        [ Svg.line [ x1 "12", y1 "2", x2 "12", y2 "6" ] []
+        , Svg.line [ x1 "12", y1 "18", x2 "12", y2 "22" ] []
+        , Svg.line [ x1 "4.93", y1 "4.93", x2 "7.76", y2 "7.76" ] []
+        , Svg.line [ x1 "16.24", y1 "16.24", x2 "19.07", y2 "19.07" ] []
+        , Svg.line [ x1 "2", y1 "12", x2 "6", y2 "12" ] []
+        , Svg.line [ x1 "18", y1 "12", x2 "22", y2 "12" ] []
+        , Svg.line [ x1 "4.93", y1 "19.07", x2 "7.76", y2 "16.24" ] []
+        , Svg.line [ x1 "16.24", y1 "7.76", x2 "19.07", y2 "4.93" ] []
+        ]
+
+
+pause : Html msg
+pause =
+    svgFeatherIcon "pause"
+        [ Svg.rect [ Svg.Attributes.x "6", y "4", width "4", height "16" ] []
+        , Svg.rect [ Svg.Attributes.x "14", y "4", width "4", height "16" ] []
+        ]
+
+
+play : Html msg
+play =
+    svgFeatherIcon "play"
+        [ Svg.polygon [ points "5 3 19 12 5 21 5 3" ] []
+        ]
+
+
+repeat : Html msg
+repeat =
+    svgFeatherIcon "repeat"
+        [ Svg.polyline [ points "17 1 21 5 17 9" ] []
+        , Svg.path [ d "M3 11V9a4 4 0 0 1 4-4h14" ] []
+        , Svg.polyline [ points "7 23 3 19 7 15" ] []
+        , Svg.path [ d "M21 13v2a4 4 0 0 1-4 4H3" ] []
+        ]
+
+
+trash2 : Html msg
+trash2 =
+    svgFeatherIcon "trash-2"
+        [ Svg.polyline [ points "3 6 5 6 21 6" ] []
+        , Svg.path [ d "M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" ] []
+        , Svg.line [ x1 "10", y1 "11", x2 "10", y2 "17" ] []
+        , Svg.line [ x1 "14", y1 "11", x2 "14", y2 "17" ] []
+        ]
+
+
+video : Html msg
+video =
+    svgFeatherIcon "video"
+        [ Svg.polygon [ points "23 7 16 12 23 17 23 7" ] []
+        , Svg.rect [ Svg.Attributes.x "1", y "5", width "15", height "14", rx "2", ry "2" ] []
+        ]
